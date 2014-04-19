79113ec23bd83dd00df7cf747101cb23832c9739
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-04-19 23:18:20 +0200

    various tweaks

--- Helper/LocalLinkResolver.php
+++ Helper/LocalLinkResolver.php
@@ -2,11 +2,12 @@
 
 namespace Prismic\Bundle\PrismicBundle\Helper;
 
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
 use Prismic\Api;
 use Prismic\Ref;
 use Prismic\LinkResolver;
+use Prismic\Fragment\Link\DocumentLink;
 
 class LocalLinkResolver extends LinkResolver
 {
@@ -15,16 +16,25 @@ class LocalLinkResolver extends LinkResolver
     private $api;
     private $maybeRef;
 
-    public function __construct(Router $router, Api $api, Ref $maybeRef = null)
+    /**
+     * @param RouterInterface $router
+     * @param Api $api
+     * @param string|null $maybeRef
+     */
+    public function __construct(RouterInterface $router, Api $api, $maybeRef = null)
     {
         $this->router = $router;
         $this->api = $api;
         $this->maybeRef = $maybeRef;
     }
 
+    /**
+     * @param DocumentLink $link
+     * @return string
+     */
     public function resolve($link) 
     {
-        return $this->router->generate('detail', array('id' => $link->getId(), 'slug' => $link->getSlug(), 'ref' => $this->maybeRef));
+        return $this->router->generate('detail', array('id' => $link->getId(), 'slug' => $link->getSlug(), 'ref' => (string) $this->maybeRef));
     }
 
 }
--- Helper/PrismicContext.php
+++ Helper/PrismicContext.php
@@ -2,8 +2,11 @@
 
 namespace Prismic\Bundle\PrismicBundle\Helper;
 
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
+use Prismic\api;
+use Prismic\Ref;
+use Prismic\Document;
 use Prismic\Fragment\Link\DocumentLink;
 
 class PrismicContext
@@ -16,28 +19,49 @@ class PrismicContext
     private $masterRef;
     private $ref;
     private $maybeRef;
+    private $linkResolver;
 
-    public function __construct(PrismicHelper $prismic, Router $router)
+    /**
+     * @param PrismicHelper $prismic
+     * @param RouterInterface $router
+     */
+    public function __construct(PrismicHelper $prismic, RouterInterface $router)
     {
         $this->prismic = $prismic;
         $this->router = $router;
     }
 
+    /**
+     * @param string $accessToken
+     */
     public function setAccessToken($accessToken)
     {
         $this->accessToken = $accessToken;
+
+        $this->api = $this->linkResolver = null;
     }
 
+    /**
+     * @param string $ref
+     */
     public function setRef($ref)
     {
         $this->ref = $ref;
+
+        $this->maybeRef = $this->linkResolver = null;
     }
 
+    /**
+     * @return bool
+     */
     public function hasPrivilegedAccess()
     {
         return isset($this->accessToken);
     }
 
+    /**
+     * @return Api
+     */
     public function getApi()
     {
         if (!$this->api) {
@@ -47,6 +71,9 @@ class PrismicContext
         return $this->api;
     }
 
+    /**
+     * @return Ref
+     */
     public function getRef()
     {
         if (null === $this->ref) {
@@ -56,6 +83,9 @@ class PrismicContext
         return $this->ref;
     }
 
+    /**
+     * @return Ref
+     */
     public function getMasterRef()
     {
         if (null === $this->masterRef) {
@@ -65,27 +95,47 @@ class PrismicContext
         return $this->masterRef;
     }
 
+    /**
+     * @return Ref
+     */
     public function getMaybeRef()
     {
         if (!$this->maybeRef) {
-            $this->maybeRef = $this->getRef() === $this->getMasterRef() ? null : $this->getRef();
+            $this->maybeRef = (string) $this->getRef() === (string) $this->getMasterRef() ? null : $this->getRef();
         }
 
         return $this->maybeRef;
     }
 
-    public function linkResolver() 
+    /**
+     * @return LocalLinkResolver
+     */
+    public function getLinkResolver()
     {
-        return new LocalLinkResolver($this->router, $this->getApi(), $this->getMaybeRef());
+        if (!$this->linkResolver) {
+            $this->linkResolver = new LocalLinkResolver($this->router, $this->getApi(), $this->getMaybeRef());
+        }
+
+        return $this->linkResolver;
     }
 
-    public function resolveLink($doc) 
+    /**
+     * @param Document $doc
+     *
+     * @return string
+     */
+    public function resolveLink(Document $doc)
     {
         $link = new DocumentLink($doc->getId(), $doc->getType(), $doc->getTags(), $doc->getSlug(), false);
 
-        return $this->linkResolver()->resolve($link);
+        return $this->getLinkResolver()->resolve($link);
     }
 
+    /**
+     * @param $id
+     *
+     * @return Document|null
+     */
     public function getDocument($id) 
     {
         $docs = $this->getApi()->forms()->everything->ref($this->getRef())->query(
