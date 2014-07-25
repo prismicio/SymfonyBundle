2a328051cd8c49def2619b56ad757ddf4e7fec5d
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-07-25 14:40:04 +0200

    The OAuthController is now a service so it can be reused by non-SF apps (Silex for example).

--- Controller/OAuthController.php
+++ Controller/OAuthController.php
@@ -2,40 +2,72 @@
 
 namespace Prismic\Bundle\PrismicBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Prismic\Bundle\PrismicBundle\Helper\PrismicHelper;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class OAuthController extends Controller
+class OAuthController
 {
+    /**
+     * @var PrismicHelper
+     */
+    private $prismic;
+
+    /**
+     * @var string
+     */
+    private $redirectRoute;
+
+    /**
+     * @var array
+     */
+    private $redirectRouteParams;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * Constructor.
+     *
+     * @param PrismicHelper         $prismic
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param string                $redirectRoute
+     * @param array                 $redirectRouteParams
+     */
+    public function __construct(PrismicHelper $prismic, UrlGeneratorInterface $urlGenerator, $redirectRoute, array $redirectRouteParams = array())
+    {
+        $this->prismic = $prismic;
+        $this->urlGenerator = $urlGenerator;
+        $this->redirectRoute = $redirectRoute;
+        $this->redirectRouteParams = $redirectRouteParams;
+    }
+
     public function signinAction()
     {
-        $prismic = $this->get('prismic.helper');
-
-        return $this->redirect(
-            $prismic->getApiHome()->oauthInitiateEndpoint().'?'.http_build_query(
-                array(
-                    "client_id" => $prismic->getClientId(),
-                    "redirect_uri" => $this->generateUrl('auth_callback', array(), true),
-                    "scope" => "master+releases"
-                )
+        return new RedirectResponse($this->prismic->getApiHome()->oauthInitiateEndpoint().'?'.http_build_query(
+            array(
+                'client_id' => $this->prismic->getClientId(),
+                'redirect_uri' => $this->urlGenerator->generate('auth_callback', array(), true),
+                'scope' => "master+releases"
             )
-        );
+        ));
     }
 
     public function callbackAction(Request $request)
     {
-        $prismic = $this->get('prismic.helper');
-
         $data = array(
-            "grant_type" => "authorization_code",
-            "code" => $request->query->get('code'),
-            "redirect_uri" => $this->generateUrl('auth_callback', array(), true),
-            "client_id" => $prismic->getClientId(),
-            "client_secret" => $prismic->getClientSecret()
+            'grant_type' => 'authorization_code',
+            'code' => $request->query->get('code'),
+            'redirect_uri' => $this->urlGenerator->generate('auth_callback', array(), true),
+            'client_id' => $this->prismic->getClientId(),
+            'client_secret' => $this->prismic->getClientSecret()
         );
 
         $conn = curl_init();
-        curl_setopt($conn, CURLOPT_URL, $prismic->getApiHome()->oauthTokenEndpoint());
+        curl_setopt($conn, CURLOPT_URL, $this->prismic->getApiHome()->oauthTokenEndpoint());
         curl_setopt($conn, CURLOPT_POST, true);
         curl_setopt($conn, CURLOPT_POSTFIELDS, $data);
         curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
@@ -44,24 +76,20 @@ class OAuthController extends Controller
 
         $request->getSession()->set('ACCESS_TOKEN', $accessToken);
 
-        return $this->redirect(
-            $this->generateUrl(
-                $this->container->getParameter('prismic.oauth.redirect_route'),
-                $this->container->getParameter('prismic.oauth.redirect_route_params')
-            )
-        );
+        return new RedirectResponse($this->urlGenerator->generate(
+            $this->redirectRoute,
+            $this->redirectRouteParams
+        ));
     }
 
     public function signoutAction(Request $request)
     {
         $request->getSession()->clear();
 
-        return $this->redirect(
-            $this->generateUrl(
-                $this->container->getParameter('prismic.oauth.redirect_route'),
-                $this->container->getParameter('prismic.oauth.redirect_route_params')
-            )
-        );
+        return new RedirectResponse($this->urlGenerator->generate(
+            $this->redirectRoute,
+            $this->redirectRouteParams
+        ));
     }
 
 }
